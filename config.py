edbdbab83844cd2e7ee5f7c9ff0a50e66a586f5a
Xin <frances.xliang@gmail.com>
2020-02-08 15:22:12 +1100

    Set lazy-apps in uwsgi

--- config.py
+++ config.py
@@ -2,9 +2,12 @@ import os
 
 # Model configs
 model_dir = "/app/deepspeech-0.6.1-models"
+#model_dir = "deepspeech-0.6.1-models"
 
 
 # Annotation configs
 clip_output_path = os.path.expanduser("/app/outputs/clips")
 annotation_output_path = os.path.expanduser("/app/outputs/")
+#clip_output_path = os.path.expanduser("outputs/clips")
+#annotation_output_path = os.path.expanduser("outputs/")
 
