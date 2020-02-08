cdc3f95c9f5c3f88ccd5f621df589afd9358ed51
Xin <frances.xliang@gmail.com>
2020-02-08 15:24:14 +1100

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+
+# Download pre-trained English model and extract
+curl -LO https://github.com/mozilla/DeepSpeech/releases/download/v0.6.1/deepspeech-0.6.1-models.tar.gz
+tar xvf deepspeech-0.6.1-models.tar.gz
+
+# Install sox
+brew install sox
