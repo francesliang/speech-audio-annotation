81b89c653837bc7847330e16a0137b0b3029aa40
Xin <frances.xliang@gmail.com>
2020-02-08 15:37:27 +1100

    Add module to run existing speech-recognition

---
+++ speech_recognition/google_stt.py
@@ -0,0 +1,36 @@
+from google.cloud import speech_v1
+from google.cloud.speech_v1 import enums
+import io
+
+
+def recognise(file_path, lang_code="en-US", sample_rate=16000):
+    """
+    Transcribe a short audio file using synchronous speech recognition
+    Args:
+      local_file_path Path to local audio file, e.g. /path/audio.wav
+    """
+
+    recognised_results = []
+
+    client = speech_v1.SpeechClient()
+
+    # Encoding of audio data sent. This sample sets this explicitly.
+    # This field is optional for FLAC and WAV audio formats.
+    encoding = enums.RecognitionConfig.AudioEncoding.LINEAR16
+    config = {
+        "language_code": lang_code,
+        "sample_rate_hertz": sample_rate,
+        "encoding": encoding,
+    }
+    with io.open(file_path, "rb") as f:
+        content = f.read()
+    audio = {"content": content}
+
+    response = client.recognize(config, audio)
+    for result in response.results:
+        # First alternative is the most probable result
+        alternative = result.alternatives[0]
+        recognised_results.append(alternative)
+
+    return recognised_results
+
