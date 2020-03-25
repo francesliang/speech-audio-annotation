6cba17421c55b04c470f3bfac87af20ef13f8991
Xin <frances.xliang@gmail.com>
2020-03-25 16:05:12 +1100

    Update file paths in config file

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import os
 
 
 # Annotation configs
-#clip_output_path = os.path.expanduser("/app/outputs/clips")
-#annotation_output_path = os.path.expanduser("/app/outputs/")
-clip_output_path = os.path.expanduser("outputs/clips")
-annotation_output_path = os.path.expanduser("outputs/")
+clip_output_path = os.path.expanduser("/app/outputs/clips")
+annotation_output_path = os.path.expanduser("/app/outputs/")
+#clip_output_path = os.path.expanduser("outputs/clips")
+#annotation_output_path = os.path.expanduser("outputs/")
 
 
 # Model configs
-#model_dir = "/app/deepspeech-0.6.1-models"
-model_dir = "deepspeech-0.6.1-models"
+model_dir = "/app/outputs/deepspeech-0.6.1-models"
+#model_dir = "outputs/deepspeech-0.6.1-models"
 n_hidden = 2048
 checkpoint_dir = model_dir
 epochs = 3
@@ -19,4 +19,4 @@ dev_files = os.path.join(clip_output_path, "dev.csv")
 test_files = os.path.join(clip_output_path, "test.csv")
 learning_rate = 0.0001
 export_dir = model_dir
-deepspeech_path = os.path.expanduser("~/Projects/DeepSpeech")
+deepspeech_path = "/app/models/deepspeech/DeepSpeech"
