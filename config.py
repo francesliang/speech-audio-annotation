edecc44b19e8227963eb5744ee5701aaf0894db2
Xin <frances.xliang@gmail.com>
2020-04-11 11:25:45 +1000

    Update model configs

--- config.py
+++ config.py
@@ -2,15 +2,13 @@ import os
 
 
 # Annotation configs
-clip_output_path = os.path.expanduser("/app/outputs/clips")
-annotation_output_path = os.path.expanduser("/app/outputs/")
-#clip_output_path = os.path.expanduser("outputs/clips")
-#annotation_output_path = os.path.expanduser("outputs/")
-
+outputs_dir = "/app/outputs/"
+# outputs_dir = "outputs/"
+clip_output_path = os.path.join(outputs_dir, "clips")
+annotation_output_path = outputs_dir
 
 # Model configs
-model_dir = "/app/outputs/deepspeech-0.6.1-models"
-#model_dir = "outputs/deepspeech-0.6.1-models"
+model_dir = os.path.join(outputs_dir, "deepspeech-0.6.1-models")
 n_hidden = 2048
 checkpoint_dir = model_dir
 epochs = 3
@@ -18,5 +16,5 @@ train_files = os.path.join(clip_output_path, "train.csv")
 dev_files = os.path.join(clip_output_path, "dev.csv")
 test_files = os.path.join(clip_output_path, "test.csv")
 learning_rate = 0.0001
-export_dir = model_dir
+export_dir = os.path.join(outputs_dir, "trained_models")
 deepspeech_path = "/app/models/deepspeech/DeepSpeech"
